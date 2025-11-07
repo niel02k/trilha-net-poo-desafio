220cb574b949b87d5c3b5429fe6d56d3eac403f4
Marcos Daniel <nielfreitasfree@gmail.com>
2025-11-07 19:52:34 -0300

    Desafio realizado com sucesso !!

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,19 @@
+
+using DesafioPOO.Models;
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+
+         public Iphone(string numero ,string modelo , string gmail, int memoria ) : base(numero, modelo, gmail, memoria)    
+    {
+           
+            
+    }
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o aplicativo {nomeApp} no iPhone...");
+        }   
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,25 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+namespace DesafioPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Console.WriteLine("Smartphone Nokia: ");    
+            Nokia nokia = new Nokia("123456789" , "Nokia 3310",  "nokia123@gmail.com ",  16 );
+            nokia.Ligar();
+            nokia.InstalarAplicativo("WhatsApp");
+ 
+
+           Console.WriteLine("\n");
+
+            Console.WriteLine("Smartphone Iphone:");   
+            Iphone iphone = new Iphone("987654321" , "Iphone 13",  "Iphon123@gmail.com  ", 128 );
+            iphone.Ligar();
+            iphone.InstalarAplicativo("Instagram"); 
+
+        
+        }
+    }
+}
